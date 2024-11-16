870ec5b18136411e8171d867ff66e8b55ffdb495
Matheus Antonio <dev.matheus.antonio@outlook.com>
2024-11-16 00:19:22 -0300

    validando os digitos

--- imc_calc/src/components/ImcCalc.jsx
+++ imc_calc/src/components/ImcCalc.jsx
@@ -12,6 +12,19 @@ const ImcCalc = () => {
     setWeight("");
   };
 
+  const validDigits = (text) => {
+    return text.replace(/[^0-9,]/g, "")
+  }
+
+  const handleHeightChange = (e) => {
+    const updateValue = validDigits(e.target.value)
+    setHeight(updateValue)
+  }
+  const handleWeightChange = (e) => {
+    const updateValue = validDigits(e.target.value)
+    setWeight(updateValue)
+  }
+
   return (
     <div id="imcContainer">
       <h2>Calculadora de IMC</h2>
@@ -24,7 +37,7 @@ const ImcCalc = () => {
               name="height"
               id="height"
               placeholder="Exemplo 1,75"
-              onChange={(e) => setHeight(e.target.value)}
+              onChange={(e) => handleHeightChange(e)}
               value={height}
             />
           </div>
@@ -35,7 +48,7 @@ const ImcCalc = () => {
               name="weight"
               id="weight"
               placeholder="Exemplo 75,5"
-              onChange={(e) => setWeight(e.target.value)}
+              onChange={(e) => handleWeightChange(e)}
               value={weight}
             />
           </div>
